8f57a5c284e1f7f738f5d50c396f965d05649b11
oyoungsun <oyoungsun1030@gmail.com>
2023-11-11 15:05:41 +0900

    feat(OrderItem) : 주문 내역을 구성하는 주문item 객체 생성 기능 구현

--- src/main/java/christmas/domain/OrderItem.java
+++ src/main/java/christmas/domain/OrderItem.java
@@ -1,4 +1,20 @@
 package christmas.domain;
 
+import christmas.utils.validators.OrderItemValidator;
+
 public class OrderItem {
+    private final Menu menu;
+    private final int count;
+
+    private OrderItem(final Menu menu, final int count) {
+        this.menu = menu;
+        this.count = count;
+    }
+
+    public static OrderItem of(final String menuName, final int count, OrderItemValidator orderItemValidator){
+        orderItemValidator.validate(menuName, count);
+        Menu menu = Menu.findMenuByName(menuName);
+        return new OrderItem(menu, count);
+    }
+
 }
--- src/test/java/christmas/TestFactory.java
+++ src/test/java/christmas/TestFactory.java
@@ -1,11 +1,13 @@
 package christmas;
 
 import christmas.domain.OrderItem;
+import christmas.utils.validators.OrderItemValidator;
 import java.util.List;
 
 public class TestFactory {    //테스트에 필요한 객체를 생성한다.
     public static List<OrderItem> createOrderItems() {
-        return List.of(new OrderItem());
+        OrderItemValidator validator = new OrderItemValidator();
+        return List.of(OrderItem.of("초코케이크", 1, validator));
     }
 
 }
