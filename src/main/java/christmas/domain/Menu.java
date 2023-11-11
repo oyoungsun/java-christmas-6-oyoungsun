a7366e684406ef5138a561016f55f460c2e5d793
oyoungsun <oyoungsun1030@gmail.com>
2023-11-11 12:00:07 +0900

    feat (Menu) : 메뉴 정보와 타입을 가진 객체 생성

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,24 @@
+package christmas.domain;
+
+public enum Menu {
+    SOUP("양송이수프", 6000),
+    TAPAS("타파스", 5500),
+    SALAD("시저샐러드",8000),
+    STEAK("티본스테이크", 55000),
+    RIBS("바비큐립", 54000),
+    SEAFOOD_PASTA("해산물파스타",35000),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000),
+    CAKE("초코케이크", 15000),
+    ICECREAM("아이스크림", 5000),
+    COLA("제로콜라", 3000),
+    WINE("레드와인", 60000),
+    CHAMPAGNE("샴페인", 25000);
+
+    private String name;
+    private int price;
+
+    private Menu(final String name, final int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
---
+++ src/main/java/christmas/domain/MenuType.java
@@ -0,0 +1,18 @@
+package christmas.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum MenuType {
+    APPETIZER(Arrays.asList(Menu.SOUP, Menu.TAPAS, Menu.SALAD)),
+    MAIN(Arrays.asList(Menu.STEAK, Menu.RIBS, Menu.SEAFOOD_PASTA, Menu.CHRISTMAS_PASTA)),
+    DESSERT(Arrays.asList(Menu.CAKE, Menu.ICECREAM)),
+    DRINK(Arrays.asList(Menu.COLA, Menu.WINE, Menu.CHAMPAGNE));
+
+    private List<Menu> menuGroup;
+
+    private MenuType(final List<Menu> menuGroup) {
+        this.menuGroup = menuGroup;
+    }
+
+}
