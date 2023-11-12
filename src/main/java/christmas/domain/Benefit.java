42b78980f2e104315e113e294b202b87c62dda46
oyoungsun <oyoungsun1030@gmail.com>
2023-11-12 19:27:22 +0900

    feat(Benefit) : 증정 여부를 가지는 객체 생성

---
+++ src/main/java/christmas/domain/Benefit.java
@@ -0,0 +1,23 @@
+package christmas.domain;
+
+public class Benefit {
+    private static final String GIFT = "샴페인 1개";
+    private static final String NOTHING = "없음";
+    private final boolean isGift;
+
+    public Benefit(final boolean isGift) {
+        this.isGift = isGift;
+    }
+
+    public static Benefit from(final boolean isGift) {
+        return new Benefit(isGift);
+    }
+
+    @Override
+    public String toString() {
+        if(isGift){
+            return GIFT;
+        }
+        return NOTHING;
+    }
+}
