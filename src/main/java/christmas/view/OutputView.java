13e31ea1b1893cf82ea45bb0a1544e7c673d5df4
oyoungsun <oyoungsun1030@gmail.com>
2023-11-14 12:54:10 +0900

    feat(OutputView) : 안내메세지를 출력하는 기능 작성

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,6 @@
 package christmas.view;
 
+import christmas.constants.StringConstants;
 import christmas.domain.Benefit;
 import christmas.domain.dto.ActualPayDto;
 import christmas.domain.dto.BenefitDto;
@@ -10,6 +11,8 @@ import christmas.domain.dto.PayAmountDto;
 import christmas.domain.dto.TotalDiscountDto;
 
 public class OutputView {
+    private static final String HELLO = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String EXPECTED_EVENT = "12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String ORDER_MENU = "<주문 메뉴>";
     private static final String TOTAL_PAY_AMOUNT = "<할인 전 총주문 금액>";
     private static final String BENEFIT_MENU = "<증정 메뉴>";
@@ -19,39 +22,50 @@ public class OutputView {
 
     private static final String EVENT_BADGY = "<12월 이벤트 배지>";
 
+    public static void printHello(){
+        System.out.println(HELLO);
+    }
 
+    public static void printExpectEvent() {
+        System.out.println(EXPECTED_EVENT);
+    }
 
-    public void printOrderMenu(OrderOuputDto orders){
+    public static void printOrderMenu(OrderOuputDto orders){
         System.out.println(ORDER_MENU);
         System.out.println(orders.getOrders());
     }
-    public void printTotalPayAmonut(PayAmountDto amonut){
+    public static void printTotalPayAmonut(PayAmountDto amonut){
         System.out.println(TOTAL_PAY_AMOUNT);
         System.out.println(amonut.getTotalPayAmount());
     }
 
-    public void printBenefit(BenefitDto benefit){
+    public static void printBenefit(BenefitDto benefit){
         System.out.println(BENEFIT_MENU);
         System.out.println(benefit.getBenefit());
     }
 
-    public void printDiscount(DiscountDto discount){
+    public static void printDiscount(DiscountDto discount){
         System.out.println(DISCOUNT_MENU);
         System.out.println(discount.getTotalDiscount());
     }
 
-    public void printTotalDiscountAmonut(TotalDiscountDto totalDiscount){
+    public static void printTotalDiscountAmonut(TotalDiscountDto totalDiscount){
         System.out.println(TOTAL_DISCOUNT_AMOUNT);
         System.out.println(totalDiscount.getTotalDiscountDto());
     }
 
-    public void printActualPayAmount(ActualPayDto actualPayDto){
+    public static void printActualPayAmount(ActualPayDto actualPayDto){
         System.out.println(ACTUAL_PAY_AMOUNT);
         System.out.println(actualPayDto.getActualPayAmount());
     }
 
+
     public void printEventBadgy(EventDto event){
         System.out.println(EVENT_BADGY);
         System.out.println(event.getBadgy());
+    }
+
+    private static void printEmpty() {
+        System.out.println(StringConstants.ENTER);
     }
 }
