77bbe44098b2d4679aea5dd9bf292a002210625f
oyoungsun <oyoungsun1030@gmail.com>
2023-11-14 12:54:52 +0900

    feat(EventPlannerController) : 컨트롤러 객체를 생성하는 기능 구현

---
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -0,0 +1,17 @@
+package christmas.controller;
+
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class EventPlannerController {
+    private final InputView inputView;
+    public EventPlannerController(final InputView inputView){
+        this.inputView = inputView;
+    }
+
+    public void run(){
+
+    }
+
+
+}
