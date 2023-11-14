8aad9db490a80660af82634ae04946f479920a97
oyoungsun <oyoungsun1030@gmail.com>
2023-11-14 13:40:13 +0900

    feat(Controller) : 미션 수행 흐름 구조 구현

--- src/main/java/christmas/controller/EventPlannerController.java
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -1,17 +1,32 @@
 package christmas.controller;
 
+
+import christmas.domain.Date;
+import christmas.domain.Order;
+import christmas.service.BenefitService;
+import christmas.service.DiscountService;
 import christmas.view.InputView;
-import christmas.view.OutputView;
 
 public class EventPlannerController {
     private final InputView inputView;
+    private DiscountService discountService;
+    private BenefitService benefitService;
+    private Order order;
+    private Date date;
+    private PayAmount payAmount;
+
     public EventPlannerController(final InputView inputView){
         this.inputView = inputView;
     }
 
     public void run(){
-
+        guide();
+        int totalOrderAmount = orderMenu();
+        requestTotalOrder(totalOrderAmount);
+        discount();
+        int totalDiscountAmount = sale();
+        total();
+        badgy(totalDiscountAmount);
     }
-
 
 }
