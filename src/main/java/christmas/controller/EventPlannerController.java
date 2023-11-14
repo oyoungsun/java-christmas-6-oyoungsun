55720768ecf403914ffa754522332e2588732d57
oyoungsun <oyoungsun1030@gmail.com>
2023-11-14 13:47:47 +0900

    feat(Controller) : view-domain 연결 기능 구현

--- src/main/java/christmas/controller/EventPlannerController.java
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -1,11 +1,18 @@
 package christmas.controller;
 
-
 import christmas.domain.Date;
+import christmas.domain.Event;
 import christmas.domain.Order;
+import christmas.domain.PayAmount;
+import christmas.domain.dto.ActualPayDto;
+import christmas.domain.dto.EventDto;
+import christmas.domain.dto.OrderOuputDto;
+import christmas.domain.dto.PayAmountDto;
+import christmas.domain.dto.TotalDiscountDto;
 import christmas.service.BenefitService;
 import christmas.service.DiscountService;
 import christmas.view.InputView;
+import christmas.view.OutputView;
 
 public class EventPlannerController {
     private final InputView inputView;
@@ -20,13 +27,60 @@ public class EventPlannerController {
     }
 
     public void run(){
-        guide();
+        guideOrderInformation();
         int totalOrderAmount = orderMenu();
-        requestTotalOrder(totalOrderAmount);
+        PayAmount payAmount = new PayAmount(totalOrderAmount);
+        requestTotalOrder();
+        discountService = DiscountService.from(payAmount, date);
+        benefitService = BenefitService.from(payAmount);
         discount();
         int totalDiscountAmount = sale();
         total();
         badgy(totalDiscountAmount);
+    }
+        private void guideOrderInformation() {
+        OutputView.printHello();
+        date = inputView.readDate();
+        order = inputView.readOrder();
+        OutputView.printExpectEvent();
+
+    }
+
+    private int orderMenu() {
+        OrderOuputDto orderOuputDto = OrderOuputDto.fromOrder(order);
+        OutputView.printOrderMenu(orderOuputDto);
+        return order.requestTotalPrice();
+    }
+    private void requestTotalOrder() {
+        PayAmountDto payAmonutDto = PayAmountDto.fromPayAmonut(payAmount);
+        OutputView.printTotalPayAmount(payAmonutDto);
+    }
+
+    private void discount() { // 증정, 혜택, 할
+        int mainCount = order.requestMainCount();
+        int dessertCount = order.requestDessertCount();
+        boolean isGift = benefitService.isGift();
+        discountService.discount(mainCount, dessertCount, isGift);
+    }
+
+    private int sale() {
+        int totalDiscountAmount = discountService.requestTotalDiscountAmount();
+        TotalDiscountDto totalDiscountDto = TotalDiscountDto.fromTotalDiscount(totalDiscountAmount);
+        OutputView.printTotalDiscountAmount(totalDiscountDto);
+        return totalDiscountAmount;
+    }
+
+    private void total() {
+        int totalActualDiscountAmount = discountService.requestActualDiscountAmount();
+        int totalPrice = order.requestTotalPrice();
+        ActualPayDto actualPayDto = ActualPayDto.from(totalPrice, totalActualDiscountAmount);
+        OutputView.printActualPayAmount(actualPayDto);
+    }
+
+    private void badgy(final int totalDiscountAmount) {
+        Event event = new Event(totalDiscountAmount);
+        EventDto eventDto = EventDto.fromEvent(event);
+        OutputView.printEventBadgy(eventDto);
     }
 
 }
