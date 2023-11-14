7f2b95fe6fafae66d3fe1f52478a572b6cb0bf51
oyoungsun <oyoungsun1030@gmail.com>
2023-11-14 18:08:31 +0900

    refactor(InputConvertor) : 주문 내역을 변환하는 기능 리턴값 변경

--- src/main/java/christmas/utils/InputConvertor.java
+++ src/main/java/christmas/utils/InputConvertor.java
@@ -2,9 +2,12 @@ package christmas.utils;
 
 import christmas.constants.StringConstants;
 import christmas.domain.Date;
+import christmas.domain.Order;
 import christmas.domain.OrderItem;
 import christmas.utils.validators.DateValidator;
+import christmas.utils.validators.InputValidator;
 import christmas.utils.validators.OrderItemValidator;
+import christmas.utils.validators.OrderValidator;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,20 +29,21 @@ public class InputConvertor {
         }
     }
 
-    public String[] convertOrder(final String input) {
-        return input.split(COMMA);
+    public Order convertOrder(final  List<OrderItem> input, final OrderValidator validator) {
+        return Order.from(input, validator);
     }
 
-    public List<OrderItem> convertOrderItems(String[] input, final OrderItemValidator validator) {
+    public List<OrderItem> convertOrderItems(String input, final OrderItemValidator validator) {
+        String[] inputs = input.split(COMMA);
         List<OrderItem> items = new ArrayList<>();
-        for (String item : input) {
-            OrderItem orderItem = convertOrderItem(validator, item);
+        for (String item : inputs) {
+            OrderItem orderItem = convertOrderItem(item, validator);
             items.add(orderItem);
         }
         return items;
     }
 
-    private OrderItem convertOrderItem(final OrderItemValidator validator, final String item) {
+    private OrderItem convertOrderItem(final String item, final OrderItemValidator validator) {
         String[] splitedByDash = item.split(DASH);
         String menu = splitedByDash[0];
         int count = covertStringToInt(splitedByDash[1]);
--- src/test/java/christmas/utils/InputConvertorTest.java
+++ src/test/java/christmas/utils/InputConvertorTest.java
@@ -16,7 +16,7 @@ class InputConvertorTest {
     void convertOrderItems는_주어진문자열배열을_OrderItem리스트로반환한다(){
         // given
         InputConvertor convertor = new InputConvertor();
-        String [] input = {"티본스테이크-1","바비큐립-1","초코케이크-2","제로콜라-1"};
+        String input = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
         // when
         List<OrderItem> result = convertor.convertOrderItems(input, new OrderItemValidator());
         Order order = Order.from(result, new OrderValidator());
@@ -25,7 +25,22 @@ class InputConvertorTest {
                 "바비큐립 1개",
                 "초코케이크 2개",
                 "제로콜라 1개");
+    }
 
+    @Test
+    void convertOrder는_주어진OrderItem배열을_OrderItem리스트로반환한다(){
+        // given
+        InputConvertor convertor = new InputConvertor();
+        String input = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
+        List<OrderItem> items = convertor.convertOrderItems(input, new OrderItemValidator());
+        // when
+
+        Order result = convertor.convertOrder(items, new OrderValidator());
+        // then
+        assertThat(result.toString()).contains("티본스테이크 1개",
+                "바비큐립 1개",
+                "초코케이크 2개",
+                "제로콜라 1개");
     }
 
 }
